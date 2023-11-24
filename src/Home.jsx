b7cd97a31aae9518ca8c3e21fff12b3b51a98f9e
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2023-11-24 14:10:49 -0400

    sshsh

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 export default function Home(){
+    
+    const alStorage = localStorage.getItem("lista");
+    const [count, setCount] = useState(0)
+    const [conteudo, setConteudo] = useState("");
+    const [frame, setFrame] = useState("");
+    const [nome, setNome] = useState("");
+    const [video, setVideo] = useState(alStorage ? JSON.parse(alStorage) : []);
+    const [Id, setId] = useState(0)
+
+    useEffect(() => {
+        localStorage.setItem("lista", JSON.stringify(video));
+        document.title= `voce clicou  ${count} vezes`;
+    }, [video, count])
+
+   
+
+    const salvar = (e) =>{
+        e.preventDefault();
+        setVideo([...video,{
+            conteudo: conteudo,
+            frame: frame,
+            nome: nome,
+            Id: Id
+        }]);
+        setConteudo("");
+        setNome("");
+        setImagem("");
+        setId(Id+1)
+        setCount(count+1)
+    };
+
+    const apagarC = (index) =>{
+        const novalista = [...video];
+        novalista.splice(index,1);
+        setVideo(novalista);
+    };
+
+
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Login</a>
+        <a class="nav-link" href="#">Cadastrar</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>  
+
+
      );
 }
